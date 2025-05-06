cde65512980b041ec6f54644973ee7db14fe820d
jedelmansdsu <johnaedelman@gmail.com>
2025-05-05 18:17:11 -0700

    Improved compiler functionality
    Now supports the following syntax:
    int x = 5;
    int x = x + 1;
    int x = x - 2;

--- Compiler/program7.c
+++ Compiler/program7.c
@@ -1,17 +1,9 @@
-// FizzBuzz in C
+
 #include <stdio.h>
 
-int main() {  // 1
-    for (int i = 1; i <= 100; i++){  // 2
-        if (i % 5 == 0 && i % 3 == 0){  // 3
-            printf("FizzBuzz\n");  // 4
-        } else if (i % 3 == 0){  // 5
-            printf("Fizz\n");  // 6
-        } else if (i % 5 == 0){  // 7
-            printf("Buzz\n");  // 8
-        } else{  // 9
-            printf("%d\n", i);  // 10
-        }
-    }
-    return 0;  // 11
+int main(){
+    int x = 5;
+    x = x - 1;
+    int y = x;
+    printf("%d ouuughhhh %d", x, y);
 }
