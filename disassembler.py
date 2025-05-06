5d4bcf5b77756d2fa0c09e84b51f4f447d3f4182
johnaedelman <johnaedelman@gmail.com>
2025-05-06 02:11:10 -0700

    decent progress, not fully functional

--- disassembler.py
+++ disassembler.py
@@ -87,15 +87,24 @@ def handle_lines(bin_file: str, mips_file: str):
         output_file.write(instruction)
         output_file.write("\n")
 
+
+def convert_offset(offset: str, num_bits: int):
+    if offset.startswith("0"):
+        return int(offset, 2)
+    elif offset.startswith("1"):  # convert from two's complement signed
+        return int(offset, 2) - (1 << num_bits)
 # function for conversion
 def bin_to_mips(line):
     mips = []
+    line_count = 0
+    offsets = {}
     bit_string = ""
     # add every bit in line to bit_string
     for i in range(0, len(line)):
         bit_string += line[i]
         # if line has 32 bits, isolate opcode
         if len(bit_string) == 32:
+            line_count += 1
             op_code = bit_string[0:6]
             # if R-type, assign others accordingly
             if op_code == "000000":
@@ -169,16 +178,18 @@ def bin_to_mips(line):
             elif op_code == "000010":
                 offset = bit_string[6:]
                 mips.append(
-                    f"{op_code} {int(offset, 2)}"
+                    f"{op_code} {convert_offset(offset, 26)}"
                 )
+                offsets[line_count] = convert_offset(offset, 26)
                 print(f"TEST: jump instructions: {op_code} {offset} --> {op_codes[op_code]} (target address)")
 
             # check for beq and blt
-            elif op_code in ["000100"]:
+            elif op_code in ["000100", "000101"]:
                 rs, rt, offset = bit_string[6:11], bit_string[11:16], bit_string[16:32]
                 mips.append(
-                    f"{op_codes[op_code]} {registers[rs]}, {registers[rt]}, {int(offset, 2)}"
+                    f"{op_codes[op_code]} {registers[rs]}, {registers[rt]}, {convert_offset(offset, 16)}"
                 )
+                offsets[line_count] = convert_offset(offset, 16)
                 print(f"TEST: beq instructions: {op_code} {rs} {rt} {offset} --> {op_codes[op_code]} {registers[rs]} {registers[rt]} (target address)")
 
             else: # I-type instructions for others like addi
@@ -189,9 +200,17 @@ def bin_to_mips(line):
                 print(f"TEST: Other I-type instructions: {op_code} {rt} {rs} {offset} --> {op_codes[op_code]} {registers[rs]} {registers[rt]} {int(offset, 2)}")
             
             bit_string = ""
+    print(offsets)
+    label_count = 0
+    for o in offsets:
+        index = o + offsets[o] + label_count - 1
+        if not mips[index].strip("\n").endswith(":"):  # If there's no label there already
+            mips.insert(index, f"label{label_count}:")
+            label_count += 1
     return mips
 
 # if __name__ == "__main__":
     # handle_lines(sys.argv[1])
+
 
 handle_lines("custom_machine_code.txt", "BACK_TO_MIPS.txt")
